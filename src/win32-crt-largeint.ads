bb71cc840c95b8ab260174bc28c694b1ae65b303
Pascal Obry <obry@adacore.com>
2000-11-11 21:59:44 +0000

    Lots of changes to support the LARGE_INTEGER operations. All pragma import have been removed because there is no DLL for this unit it  is a Microsoft static library. Fixes 8109-003.

--- src/win32-crt-largeint.ads
+++ src/win32-crt-largeint.ads
@@ -1,201 +1,181 @@
---  $Source$
---  $Revision$ $Date$ $Author$
--------------------------------------------------------------------------------
---
---  THIS FILE AND ANY ASSOCIATED DOCUMENTATION IS PROVIDED WITHOUT CHARGE
---  "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING
---  BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR
---  FITNESS FOR A PARTICULAR PURPOSE.  The user assumes the entire risk as to
---  the accuracy and the use of this file.  This file may be used, copied,
---  modified and distributed only by licensees of Microsoft Corporation's
---  WIN32 Software Development Kit in accordance with the terms of the
---  licensee's End-User License Agreement for Microsoft Software for the
---  WIN32 Development Kit.
---
---  Copyright (c) Intermetrics, Inc. 1995
---  Portions (c) 1985-1994 Microsoft Corporation with permission.
---  Microsoft is a registered trademark and Windows and Windows NT are
---  trademarks of Microsoft Corporation.
---
--------------------------------------------------------------------------------
+-- $Source$
+-- $Revision$ $Date$ $Author$
+-- See end of file for Copyright (c) information.
 
 with Win32.Winnt;
 
 package Win32.crt.Largeint is
 
-   --  Large integer add - 64-bits + 64-bits -> 64-bits
+    -- Large integer add - 64-bits + 64-bits -> 64-bits
 
-   function LargeIntegerAdd
-     (Addend1 : in Win32.Winnt.LARGE_INTEGER;
-      Addend2 : in Win32.Winnt.LARGE_INTEGER)
-     return Win32.Winnt.LARGE_INTEGER;
+    function LargeIntegerAdd
+        (Addend1 : in Win32.Winnt.LARGE_INTEGER;
+         Addend2 : in Win32.Winnt.LARGE_INTEGER)
+         return Win32.Winnt.LARGE_INTEGER;
 
-   --  Enlarged integer multiply - 32-bits * 32-bits -> 64-bits
+    -- Enlarged integer multiply - 32-bits * 32-bits -> 64-bits
 
-   function EnlargedIntegerMultiply
-     (Multiplicand : in Win32.LONG;
-      Multiplier : in Win32.LONG)
-     return Win32.Winnt.LARGE_INTEGER;
+    function EnlargedIntegerMultiply
+        (Multiplicand : in Win32.LONG;
+         Multiplier : in Win32.LONG)
+        return Win32.Winnt.LARGE_INTEGER;
 
-   --  Unsigned enlarged integer multiply - 32-bits * 32-bits -> 64-bits
+    -- Unsigned enlarged integer multiply - 32-bits * 32-bits -> 64-bits
 
-   function EnlargedUnsignedMultiply
-     (Multiplicand : in Win32.ULONG;
-      Multiplier : in Win32.ULONG)
-     return Win32.Winnt.LARGE_INTEGER;
+    function EnlargedUnsignedMultiply
+        (Multiplicand : in Win32.ULONG;
+         Multiplier : in Win32.ULONG)
+        return Win32.Winnt.LARGE_INTEGER;
 
-   --  Enlarged integer divide - 64-bits / 32-bits > 32-bits
+    -- Enlarged integer divide - 64-bits / 32-bits > 32-bits
 
-   function EnlargedUnsignedDivide
-     (Dividend : in Win32.Winnt.ULARGE_INTEGER;
-      Divisor : in Win32.ULONG;
-      Remainder : in Win32.PULONG) return Win32.ULONG;
+    function EnlargedUnsignedDivide
+        (Dividend : in Win32.Winnt.ULARGE_INTEGER;
+         Divisor  : in Win32.ULONG;
+         Remainder: in Win32.PULONG) return Win32.ULONG;
 
-   --  Extended large integer magic divide - 64-bits / 32-bits -> 64-bits
+    -- Extended large integer magic divide - 64-bits / 32-bits -> 64-bits
+    -- NOT SUPPORTED
 
-   function ExtendedMagicDivide
-     (Dividend : in Win32.Winnt.LARGE_INTEGER;
-      MagicDivisor : in Win32.Winnt.LARGE_INTEGER;
-      ShiftCount : in Win32.Winnt.CCHAR) return Win32.Winnt.LARGE_INTEGER;
-   --  Large Integer divide - 64-bits / 32-bits -> 64-bits
+--  function ExtendedMagicDivide
+--      (Dividend    : in Win32.Winnt.LARGE_INTEGER;
+--       MagicDivisor: in Win32.Winnt.LARGE_INTEGER;
+--       ShiftCount  : in Win32.Winnt.CCHAR) return Win32.Winnt.LARGE_INTEGER;
 
-   function ExtendedLargeIntegerDivide
-     (Dividend : in Win32.Winnt.LARGE_INTEGER;
-      Divisor : in Win32.ULONG;
-      Remainder : in Win32.PULONG) return Win32.Winnt.LARGE_INTEGER;
+    -- Large Integer divide - 64-bits / 32-bits -> 64-bits
 
-   --  Large Integer divide - 64-bits / 32-bits -> 64-bits
+    function ExtendedLargeIntegerDivide
+        (Dividend : in Win32.Winnt.LARGE_INTEGER;
+         Divisor  : in Win32.ULONG;
+         Remainder: in Win32.PULONG) return Win32.Winnt.LARGE_INTEGER;
 
-   function LargeIntegerDivide
-     (Dividend : in Win32.Winnt.LARGE_INTEGER;
-      Divisor : in Win32.Winnt.LARGE_INTEGER;
-      Remainder : in Win32.Winnt.PLARGE_INTEGER)
-     return Win32.Winnt.LARGE_INTEGER;
+    -- Large Integer divide - 64-bits / 32-bits -> 64-bits
 
-   --  Extended integer multiply - 32-bits * 64-bits -> 64-bits
+    function LargeIntegerDivide
+        (Dividend : in Win32.Winnt.LARGE_INTEGER;
+         Divisor  : in Win32.Winnt.LARGE_INTEGER;
+         Remainder: in Win32.Winnt.PLARGE_INTEGER)
+        return Win32.Winnt.LARGE_INTEGER;
 
-   function ExtendedIntegerMultiply
-     (Multiplicand : in Win32.Winnt.LARGE_INTEGER;
-      Multiplier : in Win32.LONG) return Win32.Winnt.LARGE_INTEGER;
+    -- Extended integer multiply - 32-bits * 64-bits -> 64-bits
 
-   --  Large integer negation - -(64-bits)
+    function ExtendedIntegerMultiply
+        (Multiplicand: in Win32.Winnt.LARGE_INTEGER;
+         Multiplier  : in Win32.LONG) return Win32.Winnt.LARGE_INTEGER;
 
-   function LargeIntegerNegate
-     (Subtrahend : in Win32.Winnt.LARGE_INTEGER)
-     return Win32.Winnt.LARGE_INTEGER;
+    -- Large integer negation - -(64-bits)
+
+    function LargeIntegerNegate
+        (Subtrahend : in Win32.Winnt.LARGE_INTEGER)
+        return Win32.Winnt.LARGE_INTEGER;
 
 
-   --  Large integer subtract - 64-bits - 64-bits -> 64-bits.
+    -- Large integer subtract - 64-bits - 64-bits -> 64-bits.
 
-   function LargeIntegerSubtract
-     (Minuend : in Win32.Winnt.LARGE_INTEGER;
-      Subtrahend : in Win32.Winnt.LARGE_INTEGER)
-     return Win32.Winnt.LARGE_INTEGER;
+    function LargeIntegerSubtract
+        (Minuend    : in Win32.Winnt.LARGE_INTEGER;
+         Subtrahend : in Win32.Winnt.LARGE_INTEGER)
+        return Win32.Winnt.LARGE_INTEGER;
 
-   --  Large integer and - 64-bits & 64-bits -> 64-bits.
+    -- Large integer and - 64-bits & 64-bits -> 64-bits.
 
-   function LargeIntegerAnd
-     (Source : Win32.Winnt.LARGE_INTEGER;
-      Mask : Win32.Winnt.LARGE_INTEGER)
-     return Win32.Winnt.LARGE_INTEGER;
+    function LargeIntegerAnd
+         (Source: Win32.Winnt.LARGE_INTEGER;
+        Mask  : Win32.Winnt.LARGE_INTEGER)
+        return Win32.Winnt.LARGE_INTEGER;
 
-   --  Large integer conversion routines.
+    -- Large integer conversion routines.
 
-   --  Convert signed integer to large integer.
+    -- Convert signed integer to large integer.
 
-   function ConvertLongToLargeInteger
-     (SignedInteger : in Win32.LONG) return Win32.Winnt.LARGE_INTEGER;
+    function ConvertLongToLargeInteger
+        (SignedInteger : in Win32.LONG) return Win32.Winnt.LARGE_INTEGER;
 
-   --  Convert unsigned integer to large integer.
+    -- Convert unsigned integer to large integer.
 
-   function ConvertUlongToLargeInteger
-     (UnsignedInteger : in Win32.ULONG) return Win32.Winnt.LARGE_INTEGER;
+    function ConvertUlongToLargeInteger
+        (UnsignedInteger : in Win32.ULONG) return Win32.Winnt.LARGE_INTEGER;
 
-   --  Large integer shift routines.
+    -- Large integer shift routines.
 
-   function LargeIntegerShiftLeft
-     (LargeInteger : in Win32.Winnt.LARGE_INTEGER;
-      ShiftCount : in Win32.Winnt.CCHAR) return Win32.Winnt.LARGE_INTEGER;
+    function LargeIntegerShiftLeft
+        (LargeInteger: in Win32.Winnt.LARGE_INTEGER;
+         ShiftCount  : in Win32.Winnt.CCHAR) return Win32.Winnt.LARGE_INTEGER;
 
-   function LargeIntegerShiftRight
-     (LargeInteger : in Win32.Winnt.LARGE_INTEGER;
-      ShiftCount : in Win32.Winnt.CCHAR) return Win32.Winnt.LARGE_INTEGER;
+    function LargeIntegerShiftRight
+        (LargeInteger: in Win32.Winnt.LARGE_INTEGER;
+         ShiftCount  : in Win32.Winnt.CCHAR) return Win32.Winnt.LARGE_INTEGER;
 
-   function LargeIntegerArithmeticShift
-     (LargeInteger : in Win32.Winnt.LARGE_INTEGER;
-      ShiftCount : in Win32.Winnt.CCHAR) return Win32.Winnt.LARGE_INTEGER;
+    function LargeIntegerArithmeticShift
+        (LargeInteger: in Win32.Winnt.LARGE_INTEGER;
+         ShiftCount  : in Win32.Winnt.CCHAR) return Win32.Winnt.LARGE_INTEGER;
 
-   --  For comparison routines, define both the Win32 name and provide a
-   --  rename for convenience.
+    -- For comparison routines, define both the Win32 name and provide a
+    -- rename for convenience.
 
-   function LargeIntegerGreaterThan
-     (X, Y : Win32.Winnt.LARGE_INTEGER) return Boolean;
-   function ">"(X, Y : Win32.Winnt.LARGE_INTEGER) return Boolean
-     renames LargeIntegerGreaterThan;
+    function LargeIntegerGreaterThan
+        (X, Y: Win32.Winnt.LARGE_INTEGER) return Boolean;
+    function ">"(X, Y: Win32.Winnt.LARGE_INTEGER) return Boolean
+        renames LargeIntegerGreaterThan;
 
-   function LargeIntegerGreaterThanOrEqualTo
-     (X, Y : Win32.Winnt.LARGE_INTEGER) return Boolean;
-   function ">="(X, Y : Win32.Winnt.LARGE_INTEGER) return Boolean
-     renames LargeIntegerGreaterThanOrEqualTo;
+    function LargeIntegerGreaterThanOrEqualTo
+        (X, Y: Win32.Winnt.LARGE_INTEGER) return Boolean;
+    function ">="(X, Y: Win32.Winnt.LARGE_INTEGER) return Boolean
+        renames LargeIntegerGreaterThanOrEqualTo;
 
-   function LargeIntegerEqualTo
-     (X, Y : Win32.Winnt.LARGE_INTEGER) return Boolean;
+    function LargeIntegerEqualTo
+        (X, Y: Win32.Winnt.LARGE_INTEGER) return Boolean;
 
-   function LargeIntegerNotEqualTo
-     (X, Y : Win32.Winnt.LARGE_INTEGER) return Boolean;
+    function LargeIntegerNotEqualTo
+        (X, Y: Win32.Winnt.LARGE_INTEGER) return Boolean;
 
-   function LargeIntegerLessThan
-     (X, Y : Win32.Winnt.LARGE_INTEGER) return Boolean;
-   function "<"(X, Y : Win32.Winnt.LARGE_INTEGER) return Boolean
-     renames LargeIntegerLessThan;
+    function LargeIntegerLessThan
+        (X, Y: Win32.Winnt.LARGE_INTEGER) return Boolean;
+    function "<"(X, Y: Win32.Winnt.LARGE_INTEGER) return Boolean
+        renames LargeIntegerLessThan;
 
-   function LargeIntegerLessThanOrEqualTo
-     (X, Y : Win32.Winnt.LARGE_INTEGER) return Boolean;
-   function "<="(X, Y : Win32.Winnt.LARGE_INTEGER) return Boolean
-     renames LargeIntegerLessThanOrEqualTo;
+    function LargeIntegerLessThanOrEqualTo
+        (X, Y: Win32.Winnt.LARGE_INTEGER) return Boolean;
+    function "<="(X, Y: Win32.Winnt.LARGE_INTEGER) return Boolean
+        renames LargeIntegerLessThanOrEqualTo;
 
-   function LargeIntegerGreaterThanZero
-     (X : Win32.Winnt.LARGE_INTEGER) return Boolean;
+    function LargeIntegerGreaterThanZero
+        (X: Win32.Winnt.LARGE_INTEGER) return Boolean;
 
-   function LargeIntegerGreaterOrEqualToZero
-     (X : Win32.Winnt.LARGE_INTEGER) return Boolean;
+    function LargeIntegerGreaterOrEqualToZero
+        (X: Win32.Winnt.LARGE_INTEGER) return Boolean;
 
-   function LargeIntegerEqualToZero
-     (X : Win32.Winnt.LARGE_INTEGER) return Boolean;
+    function LargeIntegerEqualToZero
+        (X: Win32.Winnt.LARGE_INTEGER) return Boolean;
 
-   function LargeIntegerNotEqualToZero
-     (X : Win32.Winnt.LARGE_INTEGER) return Boolean;
+    function LargeIntegerNotEqualToZero
+        (X: Win32.Winnt.LARGE_INTEGER) return Boolean;
 
-   function LargeIntegerLessThanZero
-     (X : Win32.Winnt.LARGE_INTEGER) return Boolean;
+    function LargeIntegerLessThanZero
+        (X: Win32.Winnt.LARGE_INTEGER) return Boolean;
 
-   function LargeIntegerLessThanOrEqualToZero
-     (X : Win32.Winnt.LARGE_INTEGER) return Boolean;
+    function LargeIntegerLessThanOrEqualToZero
+        (X: Win32.Winnt.LARGE_INTEGER) return Boolean;
 
 private
 
-   --  Directly call Win32 API when return value is not LARGE_INTEGER
-   pragma Import (Stdcall, LargeIntegerEqualTo, "LargeIntegerEqualTo");
-   pragma Import (Stdcall, LargeIntegerNotEqualTo, "LargeIntegerNotEqualTo");
-   pragma Import (Stdcall, EnlargedUnsignedDivide, "EnlargedUnsignedDivide");
-   pragma Import (Stdcall, LargeIntegerGreaterThan, "LargeIntegerGreaterThan");
-   pragma Import (Stdcall, LargeIntegerGreaterThanOrEqualTo,
-                    "LargeIntegerGreaterThanOrEqualTo");
-   pragma Import (Stdcall, LargeIntegerLessThan, "LargeIntegerLessThan");
-   pragma Import (Stdcall, LargeIntegerLessThanOrEqualTo,
-                    "LargeIntegerLessThanOrEqualTo");
-   pragma Import (Stdcall, LargeIntegerGreaterThanZero,
-                    "LargeIntegerGreaterThanZero");
-   pragma Import (Stdcall, LargeIntegerGreaterOrEqualToZero,
-                    "LargeIntegerGreaterOrEqualToZero");
-   pragma Import (Stdcall, LargeIntegerEqualToZero, "LargeIntegerEqualToZero");
-   pragma Import (Stdcall, LargeIntegerNotEqualToZero,
-                    "LargeIntegerNotEqualToZero");
-   pragma Import (Stdcall, LargeIntegerLessThanZero,
-                    "LargeIntegerLessThanZero");
-   pragma Import (Stdcall, LargeIntegerLessThanOrEqualToZero,
-                    "LargeIntegerLessThanOrEqualToZero");
+-------------------------------------------------------------------------------
+--
+-- THIS FILE AND ANY ASSOCIATED DOCUMENTATION IS PROVIDED WITHOUT CHARGE
+-- "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING
+-- BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR
+-- FITNESS FOR A PARTICULAR PURPOSE.  The user assumes the entire risk as to
+-- the accuracy and the use of this file.  This file may be used, copied,
+-- modified and distributed only by licensees of Microsoft Corporation's
+-- WIN32 Software Development Kit in accordance with the terms of the
+-- licensee's End-User License Agreement for Microsoft Software for the
+-- WIN32 Development Kit.
+--
+-- Copyright (c) Intermetrics, Inc. 1995
+-- Portions (c) 1985-1994 Microsoft Corporation with permission.
+-- Microsoft is a registered trademark and Windows and Windows NT are
+-- trademarks of Microsoft Corporation.
+--
+-------------------------------------------------------------------------------
 
 end Win32.crt.Largeint;
-
-
