61056e8baaa04f100cbc2c4bf650feedbdc8cbf8
Pascal Obry <obry@adacore.com>
2000-11-11 22:01:23 +0000

    Lots of changes to support the LARGE_INTEGER operations. All pragma import have been removed because there is no DLL for this unit  it is a Microsoft static library. Fixes 8109-003.

--- src/win32-crt-largeint.adb
+++ src/win32-crt-largeint.adb
@@ -1,16 +1,16 @@
---  $Source$
---  $Revision$ $Date$ $Author$
+-- $Source$
+-- $Revision$ $Date$ $Author$
 -------------------------------------------------------------------------------
 --
---  THIS FILE AND ANY ASSOCIATED DOCUMENTATION IS FURNISHED "AS IS"
---  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING
---  BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY
---  AND/OR FITNESS FOR A PARTICULAR PURPOSE.  The user assumes the
---  entire risk as to the accuracy and the use of this file.
+-- THIS FILE AND ANY ASSOCIATED DOCUMENTATION IS FURNISHED "AS IS" WITHOUT
+-- WARRANTY OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+-- TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND/OR FITNESS FOR A PARTICULAR
+-- PURPOSE.  The user assumes the entire risk as to the accuracy and the
+-- use of this file.
 --
---  Copyright (c) Intermetrics, Inc. 1995
---  Royalty-free, unlimited, worldwide, non-exclusive use, modification,
---  reproduction and further distribution of this file is permitted.
+-- Copyright (c) Intermetrics, Inc. 1995
+-- Royalty-free, unlimited, worldwide, non-exclusive use, modification,
+-- reproduction and further distribution of this file is permitted.
 --
 -------------------------------------------------------------------------------
 
@@ -19,175 +19,256 @@ with Win32.crt.Largeint.Ops;
 
 package body Win32.crt.Largeint is
 
-   --  Large integer add - 64-bits + 64-bits -> 64-bits
+    -- Large integer add - 64-bits + 64-bits -> 64-bits
 
---     I : Win32.Winnt.LARGE_INTEGER;
---     ULI : Win32.Winnt.ULARGE_INTEGER;
---     UL : Win32.ULONG;
+    I :  Win32.Winnt.LARGE_INTEGER;
+    ULI: Win32.Winnt.ULARGE_INTEGER;
+    UL:  Win32.ULONG;
 
-   function LI is new Ada.Unchecked_Conversion
-     (Win32.LONGLONG, Win32.Winnt.LARGE_INTEGER);
+    function LI is new Ada.Unchecked_Conversion (
+        Win32.LONGLONG, Win32.Winnt.LARGE_INTEGER);
 
-   function LI is new Ada.Unchecked_Conversion
-     (Win32.DWORDLONG, Win32.Winnt.LARGE_INTEGER);
+    function LI is new Ada.Unchecked_Conversion (
+        Win32.DWORDLONG, Win32.Winnt.LARGE_INTEGER);
 
-   package Op renames Win32.crt.Largeint.Ops;
-   use Op;
+    package Op renames Win32.crt.Largeint.Ops;
+    use Op;
 
-   function LargeIntegerAdd
-     (Addend1 : in Win32.Winnt.LARGE_INTEGER;
-      Addend2 : in Win32.Winnt.LARGE_INTEGER)
-     return Win32.Winnt.LARGE_INTEGER is
-   begin
-      return LI (Addend1.QuadPart + Addend2.QuadPart);
-   end LargeIntegerAdd;
+    function LargeIntegerAdd
+        (Addend1 : in Win32.Winnt.LARGE_INTEGER;
+         Addend2 : in Win32.Winnt.LARGE_INTEGER)
+         return Win32.Winnt.LARGE_INTEGER is
+    begin
+        return LI(Addend1.Quadpart + Addend2.Quadpart);
+    end;
 
-   --  Enlarged integer multiply - 32-bits * 32-bits -> 64-bits
+    -- Enlarged integer multiply - 32-bits * 32-bits -> 64-bits
 
-   function EnlargedIntegerMultiply
-     (Multiplicand : in Win32.LONG;
-      Multiplier : in Win32.LONG)
-     return Win32.Winnt.LARGE_INTEGER is
-   begin
-      return LI (Op."*"(Multiplicand, Multiplier));
-   end EnlargedIntegerMultiply;
+    function EnlargedIntegerMultiply
+        (Multiplicand : in Win32.LONG;
+         Multiplier   : in Win32.LONG)
+        return Win32.Winnt.LARGE_INTEGER is
+    begin
+        return LI(OP."*"(Multiplicand, Multiplier));
+    end;
 
-   --  Unsigned enlarged integer multiply - 32-bits * 32-bits -> 64-bits
+    -- Unsigned enlarged integer multiply - 32-bits * 32-bits -> 64-bits
 
-   function EnlargedUnsignedMultiply
-     (Multiplicand : in Win32.ULONG;
-      Multiplier : in Win32.ULONG)
-     return Win32.Winnt.LARGE_INTEGER is
-   begin
-      return LI (Op."*"(Multiplicand, Multiplier));
-   end EnlargedUnsignedMultiply;
+    function EnlargedUnsignedMultiply
+        (Multiplicand : in Win32.ULONG;
+         Multiplier   : in Win32.ULONG)
+        return Win32.Winnt.LARGE_INTEGER is
+    begin
+        return LI(OP."*"(Multiplicand, Multiplier));
+    end;
 
-   --  Extended large integer magic divide - 64-bits / 32-bits -> 64-bits
+    -- Extended integer multiply - 32-bits * 64-bits -> 64-bits
 
-   function ExtendedMagicDivide
-     (Dividend : in Win32.Winnt.LARGE_INTEGER;
-      MagicDivisor : in Win32.Winnt.LARGE_INTEGER;
-      ShiftCount : in Win32.Winnt.CCHAR) return Win32.Winnt.LARGE_INTEGER is
-   begin
-      return LI (Op.ExtendedMagicDivide
-        (Dividend.QuadPart, MagicDivisor.QuadPart,
-        Win32.Winnt.CCHAR'Pos (ShiftCount)));
-   end ExtendedMagicDivide;
+    function ExtendedIntegerMultiply
+        (Multiplicand: in Win32.Winnt.LARGE_INTEGER;
+         Multiplier  : in Win32.LONG) return Win32.Winnt.LARGE_INTEGER is
+    begin
+        return LI(Multiplicand.Quadpart * Multiplier);
+    end;
 
-   --  Large Integer divide - 64-bits / 32-bits -> 64-bits
+    -- Enlarged integer divide - 64-bits / 32-bits > 32-bits
 
-   function ExtendedLargeIntegerDivide
-     (Dividend : in Win32.Winnt.LARGE_INTEGER;
-      Divisor : in Win32.ULONG;
-      Remainder : in Win32.PULONG) return Win32.Winnt.LARGE_INTEGER is
-   begin
-      return LI (Op.ExtendedLargeIntegerDivide
-        (Dividend.QuadPart, Divisor, Remainder));
-   end ExtendedLargeIntegerDivide;
+    function EnlargedUnsignedDivide
+        (Dividend : in Win32.Winnt.ULARGE_INTEGER;
+         Divisor  : in Win32.ULONG;
+         Remainder: in Win32.PULONG) return Win32.ULONG is
+    begin
+       Remainder.all := Dividend.Quadpart mod Divisor;
+       return Dividend.Quadpart / Divisor;
+    end EnlargedUnsignedDivide;
 
-   --  Large Integer divide - 64-bits / 32-bits -> 64-bits
+    -- Large Integer divide - 64-bits / 32-bits -> 64-bits
 
-   function LargeIntegerDivide
-     (Dividend : in Win32.Winnt.LARGE_INTEGER;
-      Divisor : in Win32.Winnt.LARGE_INTEGER;
-      Remainder : in Win32.Winnt.PLARGE_INTEGER)
-     return Win32.Winnt.LARGE_INTEGER is
+    function ExtendedLargeIntegerDivide
+        (Dividend : in Win32.Winnt.LARGE_INTEGER;
+         Divisor  : in Win32.ULONG;
+         Remainder: in Win32.PULONG) return Win32.Winnt.LARGE_INTEGER
+    is
+       use Win32.Crt.Largeint.Ops;
+    begin
+       Remainder.all := Dividend.Quadpart mod Divisor;
+       return LI(Dividend.Quadpart / Divisor);
+    end ExtendedLargeIntegerDivide;
 
-      RM : aliased Win32.LONGLONG;
-      Res : Win32.LONGLONG;
-   begin
-      Res := Op.LargeIntegerDivide
-        (Dividend.QuadPart, Divisor.QuadPart, RM'Access);
-      Remainder.QuadPart := RM;
-      return LI (Res);
-   end LargeIntegerDivide;
+    -- Large Integer divide - 64-bits / 32-bits -> 64-bits
 
-   --  Extended integer multiply - 32-bits * 64-bits -> 64-bits
+    function LargeIntegerDivide
+        (Dividend : in Win32.Winnt.LARGE_INTEGER;
+         Divisor  : in Win32.Winnt.LARGE_INTEGER;
+         Remainder: in Win32.Winnt.PLARGE_INTEGER)
+        return Win32.Winnt.LARGE_INTEGER is
+    begin
+       Remainder.Quadpart := Dividend.Quadpart mod Divisor.QuadPart;
+       return LI(Dividend.Quadpart / Divisor.QuadPart);
+    end LargeIntegerDivide;
 
-   function ExtendedIntegerMultiply
-     (Multiplicand : in Win32.Winnt.LARGE_INTEGER;
-      Multiplier : in Win32.LONG) return Win32.Winnt.LARGE_INTEGER is
-   begin
-      return LI (Multiplicand.QuadPart * Multiplier);
-   end ExtendedIntegerMultiply;
+    -- Large integer negation - -(64-bits)
 
-   --  Large integer negation - -(64-bits)
-
-   function LargeIntegerNegate
-     (Subtrahend : in Win32.Winnt.LARGE_INTEGER)
-     return Win32.Winnt.LARGE_INTEGER is
-   begin
-      return LI (-Subtrahend.QuadPart);
-   end LargeIntegerNegate;
+    function LargeIntegerNegate
+        (Subtrahend : in Win32.Winnt.LARGE_INTEGER)
+        return Win32.Winnt.LARGE_INTEGER is
+    begin
+        return LI(-Subtrahend.Quadpart);
+    end;
 
 
-   --  Large integer subtract - 64-bits - 64-bits -> 64-bits.
+    -- Large integer subtract - 64-bits - 64-bits -> 64-bits.
 
-   function LargeIntegerSubtract
-     (Minuend : in Win32.Winnt.LARGE_INTEGER;
-      Subtrahend : in Win32.Winnt.LARGE_INTEGER)
-     return Win32.Winnt.LARGE_INTEGER is
-   begin
-      return LI (Minuend.QuadPart - Subtrahend.QuadPart);
-   end LargeIntegerSubtract;
+    function LargeIntegerSubtract
+        (Minuend    : in Win32.Winnt.LARGE_INTEGER;
+         Subtrahend : in Win32.Winnt.LARGE_INTEGER)
+        return Win32.Winnt.LARGE_INTEGER is
+    begin
+        return LI(Minuend.Quadpart - Subtrahend.Quadpart);
+    end;
 
-   --  Large integer and - 64-bits & 64-bits -> 64-bits.
+    -- Large integer and - 64-bits & 64-bits -> 64-bits.
 
-   function LargeIntegerAnd
-     (Source : in  Win32.Winnt.LARGE_INTEGER;
-      Mask : in  Win32.Winnt.LARGE_INTEGER)
-     return Win32.Winnt.LARGE_INTEGER is
-   begin
-      return LI (To_Unsigned (Source.QuadPart) and
-                 To_Unsigned (Mask.QuadPart));
-   end LargeIntegerAnd;
+    function LargeIntegerAnd (
+         Source: in  Win32.Winnt.LARGE_INTEGER;
+         Mask  : in  Win32.Winnt.LARGE_INTEGER)
+        return Win32.Winnt.LARGE_INTEGER is
+    begin
+        return LI(To_Unsigned(Source.Quadpart) and To_Unsigned(Mask.Quadpart));
+    end;
 
-   --  Large integer conversion routines.
+    -- Large integer conversion routines.
 
-   --  Convert signed integer to large integer.
+    -- Convert signed integer to large integer.
 
-   function ConvertLongToLargeInteger
-     (SignedInteger : in Win32.LONG) return Win32.Winnt.LARGE_INTEGER is
-      Res : Op.LONGLONG := To_64 (To_Unsigned (SignedInteger));
-   begin
-      return LI (Res);
-   end ConvertLongToLargeInteger;
+    function ConvertLongToLargeInteger
+        (SignedInteger : in Win32.LONG) return Win32.Winnt.LARGE_INTEGER is
+        Res: Op.LONGLONG := To_64(To_Unsigned(SignedInteger));
+    begin
+        return LI(Res);
+    end;
 
-   --  Convert unsigned integer to large integer.
+    -- Convert unsigned integer to large integer.
 
-   function ConvertUlongToLargeInteger
-     (UnsignedInteger : in Win32.ULONG) return Win32.Winnt.LARGE_INTEGER is
-      Res : Op.DWORDLONG := To_64 (UnsignedInteger);
-   begin
-      return LI (To_Signed (Res));
-   end ConvertUlongToLargeInteger;
+    function ConvertUlongToLargeInteger
+        (UnsignedInteger : in Win32.ULONG) return Win32.Winnt.LARGE_INTEGER is
+        Res: Op.DWORDLONG := To_64(UnsignedInteger);
+    begin
+        return LI(To_Signed(Res));
+    end;
 
-   --  Large integer shift routines.
+    -- Large integer shift routines.
 
-   function LargeIntegerShiftLeft
-     (LargeInteger : in Win32.Winnt.LARGE_INTEGER;
-      ShiftCount : in Win32.Winnt.CCHAR) return Win32.Winnt.LARGE_INTEGER is
-   begin
-      return LI (Shift_Left (To_Unsigned (LargeInteger.QuadPart),
-        Win32.Winnt.CCHAR'Pos (ShiftCount)));
-   end LargeIntegerShiftLeft;
+    function LargeIntegerShiftLeft
+        (LargeInteger: in Win32.Winnt.LARGE_INTEGER;
+         ShiftCount  : in Win32.Winnt.CCHAR) return Win32.Winnt.LARGE_INTEGER is
+    begin
+        return LI(Shift_Left(To_Unsigned(LargeInteger.Quadpart),
+                             Win32.Winnt.CCHAR'Pos(ShiftCount)));
+    end;
 
-   function LargeIntegerShiftRight
-     (LargeInteger : in Win32.Winnt.LARGE_INTEGER;
-      ShiftCount : in Win32.Winnt.CCHAR) return Win32.Winnt.LARGE_INTEGER is
-   begin
-      return LI (Shift_Right (To_Unsigned (LargeInteger.QuadPart),
-        Win32.Winnt.CCHAR'Pos (ShiftCount)));
-   end LargeIntegerShiftRight;
+    function LargeIntegerShiftRight
+        (LargeInteger: in Win32.Winnt.LARGE_INTEGER;
+         ShiftCount  : in Win32.Winnt.CCHAR) return Win32.Winnt.LARGE_INTEGER is
+    begin
+        return LI(Shift_Right(To_Unsigned(LargeInteger.Quadpart),
+                              Win32.Winnt.CCHAR'Pos(ShiftCount)));
+    end;
 
-   function LargeIntegerArithmeticShift
-     (LargeInteger : in Win32.Winnt.LARGE_INTEGER;
-      ShiftCount : in Win32.Winnt.CCHAR) return Win32.Winnt.LARGE_INTEGER is
-   begin
-      return LI (Shift_Right_Arithmetic (To_Unsigned (LargeInteger.QuadPart),
-        Win32.Winnt.CCHAR'Pos (ShiftCount)));
-   end LargeIntegerArithmeticShift;
+    function LargeIntegerArithmeticShift
+        (LargeInteger: in Win32.Winnt.LARGE_INTEGER;
+         ShiftCount  : in Win32.Winnt.CCHAR) return Win32.Winnt.LARGE_INTEGER is
+    begin
+        return LI(Shift_Right_Arithmetic(To_Unsigned(LargeInteger.Quadpart),
+                                         Win32.Winnt.CCHAR'Pos(ShiftCount)));
+    end;
+
+    --  define some LONGLONG oparators
+
+    LI_Zero : constant Win32.LONGLONG := (0, 0);
+
+    function LargeIntegerGreaterThan
+      (X, Y: Win32.Winnt.LARGE_INTEGER) return Boolean is
+    begin
+       return X.QuadPart > Y.QuadPart;
+    end LargeIntegerGreaterThan;
+
+    function LargeIntegerGreaterThanOrEqualTo
+      (X, Y: Win32.Winnt.LARGE_INTEGER) return Boolean is
+    begin
+       return X.QuadPart >= Y.QuadPart;
+    end LargeIntegerGreaterThanOrEqualTo;
+
+    function LargeIntegerEqualTo
+      (X, Y: Win32.Winnt.LARGE_INTEGER) return Boolean is
+    begin
+       return X.QuadPart = Y.QuadPart;
+    end LargeIntegerEqualTo;
+
+    function LargeIntegerNotEqualTo
+      (X, Y: Win32.Winnt.LARGE_INTEGER) return Boolean is
+    begin
+       return X.QuadPart /= Y.QuadPart;
+    end LargeIntegerNotEqualTo;
+
+    function LargeIntegerLessThan
+      (X, Y: Win32.Winnt.LARGE_INTEGER) return Boolean is
+    begin
+       return X.QuadPart < Y.QuadPart;
+    end LargeIntegerLessThan;
+
+    function LargeIntegerLessThanOrEqualTo
+        (X, Y: Win32.Winnt.LARGE_INTEGER) return Boolean is
+    begin
+       return X.QuadPart <= Y.QuadPart;
+    end LargeIntegerLessThanOrEqualTo;
+
+    function LargeIntegerGreaterThanZero
+      (X: Win32.Winnt.LARGE_INTEGER) return Boolean is
+    begin
+       return X.QuadPart > LI_Zero;
+    end LargeIntegerGreaterThanZero;
+
+    function LargeIntegerGreaterOrEqualToZero
+      (X: Win32.Winnt.LARGE_INTEGER) return Boolean is
+    begin
+       return X.QuadPart >= LI_Zero;
+    end LargeIntegerGreaterOrEqualToZero;
+
+    function LargeIntegerEqualToZero
+      (X: Win32.Winnt.LARGE_INTEGER) return Boolean is
+    begin
+       return X.QuadPart = LI_Zero;
+    end LargeIntegerEqualToZero;
+
+    function LargeIntegerNotEqualToZero
+      (X: Win32.Winnt.LARGE_INTEGER) return Boolean is
+    begin
+       return X.QuadPart /= LI_Zero;
+    end LargeIntegerNotEqualToZero;
+
+    function LargeIntegerLessThanZero
+      (X: Win32.Winnt.LARGE_INTEGER) return Boolean is
+    begin
+       return X.QuadPart < LI_Zero;
+    end LargeIntegerLessThanZero;
+
+    function LargeIntegerLessThanOrEqualToZero
+      (X: Win32.Winnt.LARGE_INTEGER) return Boolean is
+    begin
+       return X.QuadPart <= LI_Zero;
+    end LargeIntegerLessThanOrEqualToZero;
 
 end Win32.crt.Largeint;
 
 
+----------------------
+-- REVISION HISTORY --
+----------------------
+
+--  ----------------------------
+--  New changes after this line.  Each line starts with: "--  "
+--  Lots of changes to support the LARGE_INTEGER operations.
+--  All pragma import have been removed because there is no DLL for this unit
+--   it is a Microsoft static library.
+--  Fixes 8109-003.
